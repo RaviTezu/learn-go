b772d40a5ece19810288f1fd1a28e1391b59bf01
RaviTeja <rpothana@salesforce.com>
2017-02-16 20:59:12 +0530

    bufio

---
+++ playground/bufio.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		if input.Text() != "" {
+			counts[input.Text()]++
+		} else {
+			break
+		}
+	}
+
+	fmt.Println(counts)
+
+}
